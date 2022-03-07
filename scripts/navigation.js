0b2329e6c697244f16169f8e8ad415d61f3da9d7
Angel-MtzV <anm002@ucsd.edu>
2022-03-06 23:27:37 -0800

    Added Bio and nav js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,5 @@
-const Home = document.getElementById('home');
+
+const Home = document.getElementById('body');
 
 const home = document.getElementById('Home');
 const bio = document.getElementById('Bio');
@@ -11,7 +12,13 @@ const Headshot = document.getElementById('headshot');
 const Bio = document.getElementById('about');
 const Resume = document.getElementById('resume');
 const Reel = document.getElementById('reels');
-const Contact = document.getElementById('contact');
+const Contact = document.getElementById('contacting');
+
+const BioHome = document.getElementById('about-home');
+const BioBtn = document.getElementById('about-more');
+const BioContent1 = document.getElementById('about-content1');
+const BioContent2 = document.getElementById('about-content2');
+const BioContent3 = document.getElementById('about-content3');
 
 function clearActive(){
     home.className = "nav";
@@ -21,22 +28,49 @@ function clearActive(){
     contact.className = "nav";
 }
 
+function homeActive() {
+    Headshot.style.display = "";
+    Bio.style.display = "";
+    Resume.style.display = "";
+    Reel.style.display = "";
+    Contact.style.display = "";
+}
+
+function bioHomeActive() {
+    BioHome.style.display = "block";
+    BioContent1.style.display = "none";
+    BioContent2.style.display = "none";
+}
+
 home.onclick = function activate() {
     clearActive();
     home.className = "active nav";
 
     Home.className = "body home-body-grid-container";
+    Bio.className = "About home-about-grid-container"
 
-    Headshot.style.display = "";
-    Bio.style.display = "";
-    Resume.style.display = "";
-    Contact.style.display = "";
-    Reel.style.display = "";
+    homeActive();
+    bioHomeActive();
 }
 
 bio.onclick = function activate() {
     clearActive();
     bio.className = "active nav";
+
+    Home.className = "body bio-body-grid-container";
+    Bio.className = "About about-grid-container";
+
+    homeActive();
+
+    BioHome.style.display = "none";
+
+    BioContent1.style.display = "block";
+    BioContent2.style.display = "block";
+
+    Headshot.style.display = "none";
+    Resume.style.display = "none";
+    Reel.style.display = "none";
+    Contact.style.display = "none";
 }
 
 resume.onclick = function activate() {
@@ -55,6 +89,8 @@ contact.onclick = function activate() {
 
     Home.className = "body contact-body-grid-container";
 
+    homeActive();
+
     Headshot.style.display = "none";
     Bio.style.display = "none";
     Resume.style.display = "none";
@@ -63,4 +99,24 @@ contact.onclick = function activate() {
 
 ig.onclick = function Instagram() {
     window.open("https://www.instagram.com/aangelmtzz/", "_blank");
+}
+
+BioBtn.onclick = function activate() {
+    clearActive();
+    bio.className = "active nav";
+
+    Home.className = "body bio-body-grid-container";
+    Bio.className = "About about-grid-container";
+
+    homeActive();
+
+    BioHome.style.display = "none";
+
+    BioContent1.style.display = "block";
+    BioContent2.style.display = "block";
+
+    Headshot.style.display = "none";
+    Resume.style.display = "none";
+    Reel.style.display = "none";
+    Contact.style.display = "none";
 }
